9ce8165e97580e00f8246a3ca252b0e6cf1edaa7
khalid <khalid@imlink.network>
2024-02-02 11:36:41 +0300

    set application class

---
+++ app/src/main/java/com/marn/task/presentation/app/App.kt
@@ -0,0 +1,26 @@
+package com.marn.task.presentation.app
+
+import android.content.Context
+import android.content.res.Configuration
+import androidx.multidex.BuildConfig
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        MultiDex.install(this)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
